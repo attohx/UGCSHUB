51d478ce5be34d75d8cf96bb54e61eb62167f4e4
Nathan Attoh <122324996+attohx@users.noreply.github.com>
2023-11-10 15:10:29 +0000

    Add files via upload

---
+++ include/navbar.php
@@ -0,0 +1,26 @@
+<!--css file-->
+<link rel="stylesheet" href="css/style.css">
+
+<div class="head-top></div>
+
+<div id="header">
+    <div class="container"></div>
+    <div class= "main-menu">
+        <div class="menu-align"
+            <div id="logomen">
+                <a href="">Logo</a>
+            </div>
+            <nav id="nav-menucontainer">
+                <ul class="nav-menu">
+                    <li><button class="loginbtn">Buy Forms</button></li>
+                    <li><a href="#">Feed</a></li>
+                    <li><a href="course_page.php">Courses</a></li>
+                    <li><a href="contact_page.php">Contact</a></li>
+                    <li><a href="about_page.php">About</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    
+                </ul>
+            </nav>
+        </div> 
+    </div>
+</div>
