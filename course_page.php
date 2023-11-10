ca5fc07d99a3b63660f33d423bde7fc2ce9e4f99
Nathan Attoh <122324996+attohx@users.noreply.github.com>
2023-11-10 15:08:54 +0000

    Add files via upload

---
+++ course_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DCIT Course Page</title>
+    <!--css file-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php
+    @include "include/navbar.php";
+?> 
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DCIT Home Page</title>
+    <!--css file-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php
+    @include "include/navbar.php";
+?> 
+
+<section class="bannerbg">
+    <div class="overlay-bg>
+        <div class="video-box"></div>
+    </div>
+</section>
+<section class="events">
+    <div class="events-area" >
+        <div class="events-area-container" >
+            <div class="events-image" ></div>
+            <div class="events-menu-content"></div>
+        </div>
+    </div>
+</section>
+</body>
+
+</html>
