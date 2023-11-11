bb39d790218ae6db12120830ee57f77c6a73acaf
Nathan Attoh <122324996+attohx@users.noreply.github.com>
2023-11-11 05:35:09 +0000

    more content
    front page editing and css

--- index.php
+++ index.php
@@ -18,12 +18,36 @@
         <div class="video-box"></div>
     </div>
 </section>
-<section class="events">
+
+<section class="sect-gap">
     <div class="events-area" >
         <div class="events-area-container" >
             <div class="events-image" ></div>
-            <div class="events-menu-content"></div>
+            <div class="events-menu-content">
+                <div class = "title">
+                    <h1>Departmental Events</h1>
+                    <p>Programmes, Seminars, and events organised by the department</p>
+                    <a href="events.php">
+                        <b>View Upcoming and Ongoing Events</b>
+                    </a>
+                </div>
+            </div>
         </div>
+    </div>
+</section>
+
+<section class="sect-gap">
+    <div class="container">
+        <div class="popular-courses-section-align">
+            <div class="popular-courses-menu-content">
+                <div class="title">
+                    <h1>Short Courses we offer</h1>
+                    <p>Our eight week intensive certificate course is open to senior high school graduates, tertiary students, school graduates, workers and the general public.</p>
+                    <h6>Evening and weekend session only</h6>
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
 </body>
